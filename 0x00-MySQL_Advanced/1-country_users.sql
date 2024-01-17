011ad975c180cee93de1a5576f75a245d3dd012a
youssef-ait-elmahi <youssefaitelmahi3@gmail.com>
2024-01-17 18:57:16 +0100

    Add users table with country column

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,7 @@
+-- Create a table called users with the following columns:
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
+);
