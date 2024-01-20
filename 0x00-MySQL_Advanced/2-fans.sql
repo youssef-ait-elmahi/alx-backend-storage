ec694c85efa950112758fc2624b372234d64d4cc
youssef-ait-elmahi <youssefaitelmahi3@gmail.com>
2024-01-20 15:45:10 +0100

    Add query to list number of fans per origin

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,4 +1,5 @@
 -- Title: Number of fans per origin
+-- Description: List the number of fans per origin, in descending order.
 SELECT origin, SUM(nb_fans) AS nb_fans
 FROM metal_bands
 GROUP BY origin
