36f2af4b3ac2cda83b23f3fd5ae7fb5a63029051
youssef-ait-elmahi <youssefaitelmahi3@gmail.com>
2024-01-22 09:26:47 -0800

    task 8

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,1 +1,8 @@
 #!/usr/bin/env python3
+"""function that lists all documents in a collection"""
+
+def list_all(mongo_collection):
+    """ List all documents in a collection """
+    documents = mongo_collection.find()
+    return [doc for doc in documents]
+
