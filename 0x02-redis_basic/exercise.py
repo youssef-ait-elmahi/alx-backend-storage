fdae99880dcab8e5f90b21d4ba20fd2ba0bee486
youssef-ait-elmahi <youssefaitelmahi3@gmail.com>
2024-01-24 17:54:43 +0100

    Add replay method to Cache class

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -70,3 +70,15 @@ class Cache:
         """
         result = self._redis.get(key)
         return int(result)
+
+    @staticmethod
+    def replay(method: Callable):
+        """
+        Display the history of calls of a particular function
+        """
+        inputs = method.__qualname__ + ":inputs"
+        outputs = method.__qualname__ + ":outputs"
+        count = method.__self__._redis.llen(inputs)
+        print(f"{method.__qualname__} was called {count} times:")
+        for inp, out in zip(method.__self__._redis.lrange(inputs, 0, -1), method.__self__._redis.lrange(outputs, 0, -1)):
+            print(f"{method.__qualname__}{inp.decode('utf-8')} -> {out.decode('utf-8')}")
