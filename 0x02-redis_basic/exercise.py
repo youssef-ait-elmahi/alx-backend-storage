b7dba1b6e4d4cc2ccaef60156160a8ff6a3995c0
youssef-ait-elmahi <youssefaitelmahi3@gmail.com>
2024-01-25 00:48:36 +0100

    Update print statement in replay function

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -82,4 +82,4 @@ def replay(method: Callable):
     count = method.__self__._redis.llen(inputs)
     print(f"{method.__qualname__} was called {count} times:")
     for inp, out in zip(method.__self__._redis.lrange(inputs, 0, -1), method.__self__._redis.lrange(outputs, 0, -1)):
-        print(f"{method.__qualname__}{inp.decode('utf-8')} -> {out.decode('utf-8')}")
+        print(f"{method.__qualname__}(*{inp.decode('utf-8')}) -> {out.decode('utf-8')}")
