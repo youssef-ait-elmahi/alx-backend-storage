2b031fa1c27d6e4f7a50eaa69f5862dcf9005ade
youssef-ait-elmahi <youssefaitelmahi3@gmail.com>
2024-01-24 17:49:17 +0100

    Add new methods to Cache class for retrieving values from Redis

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -5,7 +5,7 @@ Module for the Cache class
 
 import redis
 import uuid
-from typing import Union
+from typing import Union, Optional, Callable
 
 class Cache:
     """
@@ -26,3 +26,26 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Optional[Callable] = None) -> Union[str, bytes, int, float]:
+        """
+        Get the value from Redis by key
+        """
+        result = self._redis.get(key)
+        if fn:
+            return fn(result)
+        return result
+
+    def get_str(self, key: str) -> str:
+        """
+        Get the value from Redis by key and convert it to a string
+        """
+        result = self._redis.get(key)
+        return result.decode("utf-8")
+
+    def get_int(self, key: str) -> int:
+        """
+        Get the value from Redis by key and convert it to an integer
+        """
+        result = self._redis.get(key)
+        return int(result)
