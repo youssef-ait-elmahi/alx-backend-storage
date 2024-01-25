a2f64ac6f3b74480727f6b477740f2bd4e414117
youssef-ait-elmahi <youssefaitelmahi3@gmail.com>
2024-01-25 01:02:23 +0100

    Refactor replay method to use method name instead of method.__qualname__

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -80,10 +80,11 @@ def replay(method: Callable):
     """
     Display the history of calls of a particular function
     """
+    method = method.__qualname__
     inputs = method.__qualname__ + ":inputs"
     outputs = method.__qualname__ + ":outputs"
     count = method.__self__._redis.llen(inputs)
     print(f"{method.__qualname__} was called {count} times:")
     for inp, out in zip(method.__self__._redis.lrange(inputs, 0, -1),
                         method.__self__._redis.lrange(outputs, 0, -1)):
-        print(f"{method.__qualname__}(*{inp.decode('utf-8')}) -> {out.decode('utf-8')}")
+        print(f"{method}(*{inp.decode('utf-8')}) -> {out.decode('utf-8')}")
